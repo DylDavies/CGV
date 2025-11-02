085f50fe85a21216333e9c2d1d2b248c0a64ecd1
Dylan Davies <34412943+DylDavies@users.noreply.github.com>
2025-11-02 21:49:11 +0200

    Remove FPS and fix favicon for hosting

--- src/systems/Stats.js
+++ src/systems/Stats.js
@@ -2,8 +2,8 @@ import Stats from 'https://unpkg.com/three@0.127.0/examples/jsm/libs/stats.modul
 
 function createStats() {
     const stats = new Stats();
-    stats.showPanel(0); // 0: fps, 1: ms, 2: mb, 3+: custom
-    document.body.appendChild(stats.dom);
+    // stats.showPanel(0); // 0: fps, 1: ms, 2: mb, 3+: custom
+    // document.body.appendChild(stats.dom);
     return stats;
 }
 
